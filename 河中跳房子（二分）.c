54e7b46a3f62e92b1c17dc9ad189dfc1c0b03a5d
zzzzzzzs <55184223+ZzzzzzzSkyward@users.noreply.github.com>
2020-04-17 14:55:42 +0000

    Create 河中跳房子（二分）.c

---
+++ 河中跳房子（二分）.c
@@ -0,0 +1,44 @@
+/*
+题目：河宽l,河里有n块石头，有一定间距。现移除至多m块石头，求所能达到的最大最小距离d。
+定义【最大最小距离】为【所有石头间最小的距离】的最大值。
+输入：l n m 数列
+输出：d
+解答：以d为变量，运用二分查找。下界为所有石头间最小的距离（不妨设为0，不影响结果）；上界为l（假设m==n,全部移除）。用一个变量ans存储找到的可行解。
+每次查找时判断是否可行：遍历所有石头，如果间距大于等于查找值，则可以跳过，否则应当移除当前石头直到距离足够，或者已经到达对岸（此时也不应该增加计数）。判断计数是否符合搬走的石头的最大值要求。
+*/
+#include <stdio.h>
+#include <stdlib.h>
+int* arr;
+int d,n,i,k,maxv,minv,ans,mid,j;
+int main()
+{
+	scanf("%d%d%d",&maxv,&d,&n);
+	d+=2;
+	arr=(int*)malloc(sizeof(int)*d);
+	arr[d-1]=maxv;
+	arr[0]=0;
+	for(i=1;i<=d;i++) scanf("%d",arr+i);
+	minv=0;
+	mid=((minv+maxv)>>1);
+	while(minv<=maxv){
+	//printf("%d %d %d\n",minv,mid,maxv);
+		j=k=0;
+		i=1;
+		while(i<=d){
+			if(arr[i]-arr[j]>=mid){
+				k++;
+				j=i;
+			}
+			i++;
+		}
+		if(k>(d-n-2)){
+			minv=mid+1;
+			ans=mid;
+		}
+		else maxv=mid-1;
+		mid=((minv+maxv)>>1);
+	}
+	printf("%d",ans);
+	return 0;
+}
+	
